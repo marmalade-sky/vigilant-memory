7afb153aafc9f647b11543be49431fc715c43e5d
Nichole <nicholebaiel@gmail.com>
2022-05-31 11:54:40 -0500

    About screen scaffolding

--- .eleventy.js
+++ .eleventy.js
@@ -4,23 +4,12 @@ module.exports = function (eleventyConfig) {
   
   eleventyConfig.addGlobalData("illustrationSrc", "/assets/svg/illustration");
   eleventyConfig.addGlobalData("iconSrc", "/assets/svg/icons");
+  eleventyConfig.addGlobalData("shapeSrc", "/assets/svg/shapes");
 
   eleventyConfig.addCollection("navigation", function (collection) {
     return collection.getFilteredByTag("pages").sort((a, b) => {
       return a.data.order - b.data.order;
     });
-  });
-
-  eleventyConfig.addNunjucksShortcode("user", function() {
-    // Available in 0.11.0 and above
-    console.log(firstName, lastName);
-    console.log( this.page );
-
-    // For example:
-    console.log( this.page.url );
-    console.log( this.page.inputPath );
-    console.log( this.page.fileSlug );
-    console.log( this.page.order);
   });
 
   return {
